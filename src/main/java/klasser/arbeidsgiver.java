0f742301b07587867119afc6c9ff4455f84c3c1c
Ampelini <s333745@oslomet.no>
2019-04-02 11:21:28 +0200

    Laget arbeidsgiver klassen

--- src/main/java/klasser/arbeidsgiver.java
+++ src/main/java/klasser/arbeidsgiver.java
@@ -1,4 +1,19 @@
 package klasser;
 
-public class arbeidsgiver {
+public class Arbeidsgiver extends Person{
+    private String orgNr;
+    private String sektor;
+    private String bransje;
+    private String navn;
+    private String[] kategori;
+
+    public Arbeidsgiver(String fornavn, String etternavn, String adresse, String postNr, String poststed, String tlf,
+                        String epost, String orgNr, String sektor, String bransje, String navn, String[] kategori){
+        super(fornavn, etternavn, adresse, postNr, poststed, tlf, epost);
+        this.orgNr = orgNr;
+        this.sektor = sektor;
+        this.bransje = bransje;
+        this.navn = navn;
+        this.kategori = kategori;
+    }
 }
