9702cf55210c89f358abb0b9e6a1b66bd3697ffe
Signe Eide <signe.eide@live.no>
2019-04-03 13:51:13 +0200

    Oppdatert regSoker.FXML med to knapper

--- src/main/java/klasser/Jobbsoker.java
+++ src/main/java/klasser/Jobbsoker.java
@@ -5,16 +5,17 @@ public class Jobbsoker extends Person {
     private String[] utdannelse;
     private String[] erfaring;
     private String[] referanser;
-    private int lønnskrav;
+    private int lonnskrav;
     private String[] kategori;
 
-    public Jobbsoker(String fornavn, String etternavn, String adresse, String postNr, String poststed, String tlf, String epost, String alder, String[] utdannelse, String[] erfaring, String[] referanser, int lønnskrav, String[] kategori) {
+    public Jobbsoker(String fornavn, String etternavn, String adresse, String postNr, String poststed, String tlf,
+                     String epost, String alder, String[] utdannelse, String[] erfaring, String[] referanser, int lonnskrav, String[] kategori) {
         super(fornavn, etternavn, adresse, postNr, poststed, tlf, epost);
         this.alder = alder;
         this.utdannelse = utdannelse;
         this.erfaring = erfaring;
         this.referanser = referanser;
-        this.lønnskrav = lønnskrav;
+        this.lonnskrav = lonnskrav;
         this.kategori = kategori;
     }
 }
