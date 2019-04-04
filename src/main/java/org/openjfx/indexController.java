6c7660b8b77ddc2e8fd20b908d7237280daf9f5a
Signe Eide <signe.eide@live.no>
2019-04-04 13:02:19 +0200

    skiftet navm til indexController

--- src/main/java/org/openjfx/indexController.java
+++ src/main/java/org/openjfx/indexController.java
@@ -12,7 +12,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 
-public class FXMLController {
+public class indexController {
 
     @FXML
     private void btnJobbsoker(ActionEvent event) {
