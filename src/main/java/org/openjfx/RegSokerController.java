7726bdd726b1001d7cc99aba89b23622811ac33b
Ampelini <s333745@oslomet.no>
2019-04-09 14:31:58 +0200

    Ordnet filechooser for registrer jobbsøker
    Endringer gjort i RegSokerController

--- src/main/java/org/openjfx/RegSokerController.java
+++ src/main/java/org/openjfx/RegSokerController.java
@@ -3,14 +3,15 @@ package org.openjfx;
 import filbehandling.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import klasser.Jobbsoker;
 import logikk.RegSokerHjelper;
 import logikk.navigeringsHjelper;
+
+import java.io.File;
+import java.io.IOException;
 
 public class RegSokerController {
 
@@ -29,17 +30,38 @@ public class RegSokerController {
 
     public void btnRegSoker(ActionEvent event) {
 
+        // FileChooser
+        Stage chooserStage = new Stage();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Lagre som");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter(".csv", "*.csv"),
+                new FileChooser.ExtensionFilter(".jobj", "*.jobj"));
+        File selectedFile = fileChooser.showSaveDialog(chooserStage);
+
         Jobbsoker nySoker = RegSokerHjelper.nySoker(txtFornavn, txtEtternavn, txtAdresse, txtPostnr, txtPoststed, txtTlf, txtEpost, txtAlder,
-                valgUtdanning, valgRetning, txtErfaring,  txtReferanse, txtLonnskrav, cbxSalg, cbxAdmin, cbxIt, cbxOkonomi);
+               valgUtdanning, valgRetning, txtErfaring, txtReferanse, txtLonnskrav, cbxSalg, cbxAdmin, cbxIt, cbxOkonomi);
 
         String ut = nySoker.toString();
 
-        new FileChooser().showSaveDialog(((MenuItem)event.getTarget()).getParentPopup().getOwnerWindow());
+        // Lagrer til .csv - - må linkes til FileChooser ?
+        LagreTilFil lagre = new LagreTilCsv();
+        try {
+            lagre.skrivPersonTilFil(ut, "./jobbsoker.csv");
+        } catch (IOException e) { // Endres til FileNotFoundException ??
+            // bør legge til en feilmeldingen i sysOut
+            e.printStackTrace();
+        }
+
+        // Henter fra .csv - må linkes til FileChooser ?
+        HenteFraCsv hentCsv = new HenteFraCsv();
+        hentCsv.henteFraFil("jobbsoker.csv");
+
 
         // Lagrer til .jobj - må linkes til FileChooser ?
         String path = "jobbsoker.jobj";
-        LagreTilJobj lagre = new LagreTilJobj();
-        lagre.skrivPersonTilFil(ut, path);
+        LagreTilJobj lagre1 = new LagreTilJobj();
+        lagre1.skrivPersonTilFil(ut, path);
 
         // Henter fra .jobj - må linkes til FileChooser ?
         HenteFraJobj hentJobj = new HenteFraJobj();
@@ -49,7 +71,7 @@ public class RegSokerController {
         //navigeringsHjelper.gåTilAnnenSide("/org/openjfx/visning.fxml", event);
     }
 
-    /*
+        /*
     public void btnRegSokerCsv(ActionEvent event) {
 
         Jobbsoker nySoker = RegSokerHjelper.nySoker(txtFornavn, txtEtternavn, txtAdresse, txtPostnr, txtPoststed, txtTlf, txtEpost, txtAlder,
@@ -72,6 +94,8 @@ public class RegSokerController {
 
         //Tar brukeren med til neste side:
         //navigeringsHjelper.gåTilAnnenSide("/org/openjfx/visning.fxml", event);
+    }
+
     }*/
 
     public void btnTilbake(ActionEvent event) {
