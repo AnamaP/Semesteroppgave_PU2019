0ce324e5b605336db25049f9d7d5b05f90ca8bca
Ampelini <s333745@oslomet.no>
2019-04-23 16:27:28 +0200

    fått til sentrering av gui vinduer

--- src/main/java/logikk/NavigeringsHjelper.java
+++ src/main/java/logikk/NavigeringsHjelper.java
@@ -22,6 +22,7 @@ public class NavigeringsHjelper {
 
             Scene scene = new Scene(parent);
             stage.setScene(scene);
+            stage.centerOnScreen();
         }
         catch (IOException io) {
             io.printStackTrace();
