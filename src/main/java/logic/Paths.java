468150751b643bf9a9502bc31a5c7cd0d0dcc6e8
Ampelini <s333745@oslomet.no>
2019-04-30 14:52:52 +0200

    navnendring på DB filene

--- src/main/java/logic/Paths.java
+++ src/main/java/logic/Paths.java
@@ -3,6 +3,6 @@ package logic;
 // Final klasse som "holder på" stiene der de forskjellige dataene skal registreres til
 // en extension med .csv/.jobj er lagt (der det er hensiktsmessig) i metoder tar inn "path"
 public final class Paths {
-    public static final String TEMPJOB = "vikariat";
-    public static final String JOBSEEKER = "jobbsoker";
+    public static final String TEMPJOB = "tempJobsDB";
+    public static final String JOBSEEKER = "jobseekersDB";
 }
